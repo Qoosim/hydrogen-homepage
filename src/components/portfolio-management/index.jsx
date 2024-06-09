7fb660d506bfc03a54806373d1cbdc6c43dd0d6a
Qoosim <ayinde.abdulghaniyy@gmail.com>
2024-06-09 10:15:41 +0100

    implement carousel section

--- src/components/portfolio-management/index.jsx
+++ src/components/portfolio-management/index.jsx
@@ -1,44 +1,85 @@
-import React from 'react'
-import ArrowRightRed from "../../assets/arrow-right-red.svg"
-import PhoneImg from "../../assets/phone-img.svg"
-import RedBoxArrow from "../../assets/red-box-arrow.svg"
-import OrangeBoxArrow from "../../assets/orange-box-arrow.svg"
+import React, { useState } from "react";
+import ArrowRightRed from "../../assets/arrow-right-red.svg";
+import NextBtnArrow from "../../assets/next-btn-arrow.svg";
+import PrevBtnArrow from "../../assets/prev-btn-arrow.svg";
+import { products } from "../../constants";
 
 export const PortfolioManagement = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleNextBtn = () => {
+    if (activeIndex < products.length - 1) {
+      setActiveIndex((prevIndex) => prevIndex + 1);
+    }
+    // setActiveIndex((prevIndex) => (prevIndex + 1) % products.length);
+  };
+
+  const handlePrevBtn = () => {
+    if (activeIndex > 0) {
+      setActiveIndex((prevIndex) => prevIndex - 1);
+    }
+    // setActiveIndex(
+    //   (prevIndex) => (prevIndex - 1 + products.length) % products.length
+    // );
+  };
+
+  const activeProduct = products[activeIndex];
+
   return (
     <>
       <section className="w-full h-full font-dmSans bg-[#130A0A] pt-[6rem]">
-        <div className='flex justify-between gap-32 px-14'>
-            <div className='flex-1 flex flex-col items-start gap-6 pt-[3rem]'>
-                <span className="text-[1.563rem] font-semibold text-[#e2d9d8]">Portfolio Management</span>
-                <p className="text-[0.875rem] text-slate-300">Managing investment portfolio for clients, which affords them the opportunity of enjoying the returns and benefits of both the capital and money markets. It cuts across Equities Investment in blue-chip quoted/unquoted companies...</p>
-                <button className='flex items-center gap-1 bg-[#fefffe] w-fit py-1.5 px-5 rounded-[0.25rem]'>
-                    <span className='text-red-500 text-sm font-medium'>Learn More</span>
-                    <img src={ArrowRightRed} alt="Arrow Right" />
-                </button>
-                <div className='flex justify-between items-center w-full'>
-                    <div className='flex items-center gap-1.5'>
-                        <div className='size-1.5 rounded-full bg-red-600'></div>
-                        <div className='size-1.5 rounded-full bg-white'></div>
-                        <div className='size-1.5 rounded-full bg-white'></div>
-                        <div className='size-1.5 rounded-full bg-white'></div>
-                        <div className='size-1.5 rounded-full bg-white'></div>
-                        <div className='size-1.5 rounded-full bg-white'></div>
-                        <div className='size-1.5 rounded-full bg-white'></div>
-                        <div className='size-1.5 rounded-full bg-white'></div>
-                        <div className='size-1.5 rounded-full bg-white'></div>
-                    </div>
-                    <div className='flex items-center gap-2'>
-                        <img src={OrangeBoxArrow} className="cursor-pointer" alt="Orange Box Arrow" />
-                        <img src={RedBoxArrow} className="cursor-pointer" alt="Red Box Arrow" />
-                    </div>
+        <div className="flex justify-between gap-32 px-14">
+          <div className="flex-1 flex flex-col items-start gap-6 pt-[3rem]">
+            <span className="text-[1.563rem] font-semibold text-[#e2d9d8] whitespace-nowrap">
+              {activeProduct.title}
+            </span>
+            <p className="text-[0.875rem] text-slate-300">
+              {activeProduct.text}
+            </p>
+            <button className="flex items-center gap-1 bg-[#fefffe] w-fit py-1.5 px-5 rounded-[0.25rem]">
+              <span className="text-red-500 text-sm font-medium">
+                {activeProduct.learMore}
+              </span>
+              <img src={ArrowRightRed} alt="Arrow Right" />
+            </button>
+            <div className="flex justify-between items-center w-full">
+              <div className="flex items-center gap-1.5">
+                {products.map((_, index) => (
+                  <div
+                    key={index}
+                    className={`size-1.5 rounded-full ${
+                      index === activeIndex ? "bg-red-600" : "bg-white"
+                    }`}
+                  ></div>
+                ))}
+              </div>
+              <div className="flex items-center gap-2">
+                <div
+                  className={`size-10 flex justify-center items-center rounded-full bg-[#eb2329] border cursor-pointer ${
+                    activeIndex === 0 ? "opacity-50 cursor-not-allowed" : ""
+                  }`}
+                  onClick={handlePrevBtn}
+                >
+                  <img src={PrevBtnArrow} alt="Previous Arrow Button" />
                 </div>
+                <div
+                  className={`size-10 flex justify-center items-center rounded-full bg-[#eb2329] border cursor-pointer ${
+                    activeIndex === products.length - 1
+                      ? "opacity-50 cursor-not-allowed"
+                      : ""
+                  }`}
+                  onClick={handleNextBtn}
+                >
+                  <img src={NextBtnArrow} alt="Next Arrow Button" />
+                </div>
+              </div>
             </div>
-            <div className='flex-1 flex justify-end'>
-                <img src={PhoneImg} alt="Phone" />
-            </div>
+          </div>
+          <div className="flex-1 flex justify-end">
+            <img src={activeProduct.img} alt="Phone" />
+          </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
--- src/constants/index.js
+++ src/constants/index.js
@@ -13,6 +13,16 @@ import StrategiesIcon3 from "../assets/strategies-icon-3.svg";
 
 import FeaturedNewsImage from "../assets/featured-news-img.svg"
 
+import PhoneImg1 from "../assets/phone-img1.svg";
+import PhoneImg2 from "../assets/phone-img2.svg";
+import PhoneImg3 from "../assets/phone-img3.svg";
+import PhoneImg4 from "../assets/phone-img4.svg";
+import PhoneImg5 from "../assets/phone-img5.svg";
+import PhoneImg6 from "../assets/phone-img6.svg";
+import PhoneImg7 from "../assets/phone-img7.svg";
+import PhoneImg8 from "../assets/phone-img8.svg";
+import PhoneImg9 from "../assets/phone-img9.svg";
+
 export const assetInvestmentData = [
   {
     id: 1,
@@ -112,3 +122,69 @@ export const featuredNewsData = [
     text: "We understand that every investor is unique, with distinct financial goals and values. That's why we offer a range of investment strategies designed. We understand that every investor is unique, with distinct financial goals and values. That's why we offer a range of investment strategies designed to cater to your individual needs and preferences."
   }
 ]
+
+export const products = [
+  {
+    id: 1,
+    title: "Portfolio Management",
+    text: "Managing investment portfolio for clients, which affords them the opportunity of enjoying the returns and benefits of both the capital and money markets. It cuts across Equities Investment in blue-chip quoted/unquoted companies...",
+    img: PhoneImg1,
+    learMore: "Learn More",
+  },
+  {
+    id: 2,
+    title: "Fund Management/Tenored Investment Plans",
+    text: "This is a short- term placement of funds with a guarantee on capital plus a pre-agreed rate of return tied to the prevailing money market rates. This offers investors attractive and fixed return on their investments from tenors ranging from 30 to 365 days. ",
+    img: PhoneImg2,
+    learMore: "Learn More",
+  },
+  {
+    id: 3,
+    title: "VAMCO Fixed Rate Note",
+    text: "A short term investment product that offers competitive guaranteed return on capital. It is a non-unitised fund that pools investor’s funds and invest in various fixed income securities. ",
+    img: PhoneImg3,
+    learMore: "Learn More",
+  },
+  {
+    id: 4,
+    title: "Dollar-Denominated Investments",
+    text: "As a way of mitigating investment risks while enhancing earnings, we invest in dollar-denominated assets (like Sovereign Eurobonds and other quoted Corporate Eurobonds e.t.c.) that help to hedge against naira depreciation and devaluation risks.",
+    img: PhoneImg4,
+    learMore: "Learn More",
+  },
+  {
+    id: 5,
+    title: "Real Estate Investments",
+    text: "We actively manage real estate assets, conduct feasibility studies, help make purchase decisions and help purchase real estate assets. ",
+    img: PhoneImg5,
+    learMore: "Learn More",
+  },
+  {
+    id: 6,
+    title: "Wealth Recoveries & Management",
+    text: "This has to do with assisting State Governments, Federal Institutions and High Networth Individuals (HNIs) to unlock liquidity from so many outstanding and hanging income/benefits and other recoverable from past investments.",
+    img: PhoneImg6,
+    learMore: "Learn More",
+  },
+  {
+    id: 7,
+    title: "VAMCO Ethical Investment Plan/Product",
+    text: "This is for ethical investors with strong religious bias. For instance, those that pay high attention/place a high premium on certain ethics, social welfare and moral standards when investing. ",
+    img: PhoneImg7,
+    learMore: "Learn More",
+  },
+  {
+    id: 8,
+    title: "Wealth Management & Advisory Services",
+    text: "Advisory services to HNI clients to enable them make the most informed investment decisions, take advantage of a wide range of our wealth management investment opportunities and offering wealthy clients advisory services...",
+    img: PhoneImg8,
+    learMore: "Learn More",
+  },
+  {
+    id: 9,
+    title: "Our Companion through Life Products",
+    text: "These products are unique and distinctive solutions, geared specifically towards clients’ specific investment objectives. This is why our approach is targeted at meeting the identified needs of various clients, at different stages of human life cycle through life. ",
+    img: PhoneImg9,
+    learMore: "Learn More",
+  },
+];
